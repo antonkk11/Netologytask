8e6c1a096a97a08591cd6accae5b86cb3554469b
Anton Kondrashov <anton_kondrashov95@mail.ru>
2024-12-06 21:19:18 +0300

    complited task 2

--- main.py
+++ main.py
@@ -10,12 +10,31 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
+    def give_lecture_grade(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer (Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+
+class Reviewer (Mentor):
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -26,19 +45,11 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    def __init__(self):
-        pass
-
-class Reviewer (Mentor):
-    def __init__(self):
-        pass
-    
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
@@ -46,3 +57,12 @@ cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+
+best_lecturer = Lecturer('Иван', 'Иванов')
+best_lecturer.courses_attached += ['Python']
+
+best_student.give_lecture_grade(best_lecturer, 'Python', 10)
+best_student.give_lecture_grade(best_lecturer, 'Python', 7)
+best_student.give_lecture_grade(best_lecturer, 'Python', 3)
+
+print(best_lecturer.grades)
